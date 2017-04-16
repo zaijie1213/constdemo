97b40d14668cb2b252207d6121b28b59826e4408
kaixi <kaixi@pkqdeMBP-2.lan>
2017-04-16 22:05:24 +0800

    init

---
+++ app/src/main/java/com/example/kaixi/constraintlayoutdemo/ConstraintActivity.java
@@ -0,0 +1,15 @@
+package com.example.kaixi.constraintlayoutdemo;
+
+import android.os.SystemClock;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class ConstraintActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_constraint);
+    }
+}
---
+++ app/src/main/java/com/example/kaixi/constraintlayoutdemo/SimpleActivity.java
@@ -0,0 +1,15 @@
+package com.example.kaixi.constraintlayoutdemo;
+
+import android.os.Bundle;
+import android.os.SystemClock;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+public class SimpleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_simple);
+    }
+}
